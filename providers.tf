54b4126a2f2c5474c4ee04e3a176519f156b6a07
Ty <tyla.boff@outlook.com>
2020-01-10 19:08:37 +0000

    updated provider versions

--- providers.tf
+++ providers.tf
@@ -1,24 +1,24 @@
 # Config
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.12.18"
 }
 
 # Providers
 provider "azurerm" {
-  version = "~> 1.39"
+  version = "~> 1.40.0"
 
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
 }
 
-provider "random" {
-  version = "~> 2.2"
+provider "null" {
+  version = "~> 2.1.0"
 }
 
 provider "local" {
-  version = "~> 1.4"
+  version = "~> 1.4.0"
 }
 
 provider "tls" {
-  version = "~> 2.1"
+  version = "~> 2.1.0"
 }
